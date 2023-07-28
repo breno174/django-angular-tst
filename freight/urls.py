57536e6bfacfc65c10d80afd09780523ca80ee4c
breno174 <breno.angelim@hotmail.com>
2023-07-28 20:11:17 -0300

    chore: update url path from freight

--- freight/urls.py
+++ freight/urls.py
@@ -2,4 +2,4 @@ from rest_framework.urls import path
 from freight.views import CreateAndListFreghtOrder
 
 
-urlpatterns = [path("/", CreateAndListFreghtOrder.as_view())]
+urlpatterns = [path("", CreateAndListFreghtOrder.as_view())]
