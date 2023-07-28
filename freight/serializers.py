aed380969140790c6a4c2d5920e0f0e23251a391
breno174 <breno.angelim@hotmail.com>
2023-07-28 20:08:30 -0300

    chore: :recycle: exclude ID field from response when create a new Freight

--- freight/serializers.py
+++ freight/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from order.serializers import OrderSerializer
 from freight.models import Freight
 
+
 class CreateNewFreight(serializers.ModelSerializer):
     order = OrderSerializer
 
     class Meta:
-        fields = "__all__"
         depth = 1
-        model=Freight
+        model = Freight
+        exclude = ["id"]
